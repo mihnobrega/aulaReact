dab8847e09ceffeb447cd525dca73e18a52a38ec
Milena <167583741+mihnobrega@users.noreply.github.com>
2025-04-08 14:55:52 -0300

    08/04/2025
    Matéria Nova-Gui

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,22 @@
+import BoasVindas from "./Exemplo1"
+
+function App() {
+ 
+  return (
+   <div>
+    <h1>Boas vindas ao React com JavaScript</h1>
+    <p>
+      Este é o arquivo App.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação React utilizando JavaScript
+    </p>
+
+    <BoasVindas nome="Milena" />
+    <BoasVindas nome="Bruna" />
+    <BoasVindas nome="Beatriz" />
+    <BoasVindas nome="Adriana" />
+    <BoasVindas nome="Ana" />
+
+   </div>
+  )
+}
+
+export default App
