578a903f62f05e49b5dd4f43581c06d362b16629
Milena <167583741+mihnobrega@users.noreply.github.com>
2025-08-05 14:53:28 -0300

    05/08
    05/08

---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,20 @@
+export default function SaoPaulo () {
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de São Paulo</h1>
+
+            <p>
+            São Paulo é o estado mais rico e populoso do Brasil, conhecido por sua forte indústria, comércio e agricultura diversificada. A capital, São Paulo, é uma metrópole global, centro financeiro e cultural do país. O estado também é famoso por suas belezas naturais, como a Serra da Mantiqueira e o litoral com praias famosas.
+            </p>
+
+            <p>
+                <img src="" />
+            </p>
+
+            <p>
+                <a href="/">Voltar para Home</a>
+            </p>
+
+        </div>
+    )
+}
